4a0154af7812e5561d38e8dd70f0f8a369c78a4b
ekazheko <kazheko@xaker.ru>
2022-04-21 18:34:46 +0300

    add command to create new delivery

---
+++ GoodsDelivery/GoodsDelivery.DeliveryWebApi/Core/Contracts/Commands/CreateDeliveryCommand.cs
@@ -0,0 +1,18 @@
+﻿namespace GoodsDelivery.DeliveryWebApi.Core.Contracts.Commands
+{
+    public class CreateDeliveryCommand
+    {
+        public CreateDeliveryCommand(string id, string number, string courierId, IEnumerable<(string, int, string)> orders)
+        {
+            Id = id;
+            Number = number;
+            CourierId = courierId;
+            Orders = orders;
+        }
+
+        public string Id { get; init; }
+        public string Number { get; init; }
+        public string CourierId { get; init; }
+        public IEnumerable<(string,int,string)> Orders { get; init; }
+    }
+}
