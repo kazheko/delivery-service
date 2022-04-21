ab5a5a4e897686425d47d74b0b0e96f52b427b4b
ekazheko <kazheko@xaker.ru>
2022-04-21 19:20:05 +0300

    add API to create delivery

---
+++ GoodsDelivery/GoodsDelivery.DeliveryWebApi/Infrastructure/Repository/DeliveryRepository.cs
@@ -0,0 +1,13 @@
+﻿using GoodsDelivery.DeliveryWebApi.Core.Contracts.Repositories;
+using GoodsDelivery.DeliveryWebApi.Core.Domain;
+
+namespace GoodsDelivery.DeliveryWebApi.Infrastructure.Repository
+{
+    public class DeliveryRepository : IDeliveryRepository
+    {
+        public Task Create(Delivery aggregate)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
--- GoodsDelivery/GoodsDelivery.DeliveryWebApi/Program.cs
+++ GoodsDelivery/GoodsDelivery.DeliveryWebApi/Program.cs
@@ -1,6 +1,21 @@
+using GoodsDelivery.DeliveryWebApi.Core.Application;
+using GoodsDelivery.DeliveryWebApi.Core.Contracts.Commands;
+using GoodsDelivery.DeliveryWebApi.Core.Contracts.Repositories;
+using GoodsDelivery.DeliveryWebApi.Infrastructure.Repository;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddScoped<DeliveryHandler>();
+builder.Services.AddScoped<IDeliveryRepository, DeliveryRepository>();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.MapPost("/", async (CreateDeliveryCommand cmd, DeliveryHandler handler) =>
+{
+    var id = await handler.Handle(cmd);
+    return Results.Created($"/{id}", null);
+});
 
 app.Run();
